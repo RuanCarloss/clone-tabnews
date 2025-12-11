9bf32dc9ca98d82c7705c41471a049be21f19900
Ruan C. <138807545+RuanCarloss@users.noreply.github.com>
2025-12-11 00:00:53 +0000

    Configuração do `databases`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
+import databases from "../../../../infra/databases.js";
+
+async function status(request, response) {
   response.status(200).json({ Chave: "Testes" });
+  const result = await databases.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
 }
 
 export default status;
